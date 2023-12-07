fb8884bd36e7b226d8cd3c243a0f5d8cb82b38d8
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-12-07 13:23:01 +0300

    improving reusability with props

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import './index.css'
 import StarRating from './components/StarRating'
 
+function Test(){
+  const [movieRating, setMovieRating] = useState(0)
+  return(
+    <div>
+      <StarRating color='blue' maxRating={10} onSetRating={setMovieRating}/>
+      <h1>This movie was rated {movieRating} stars</h1>
+    </div>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
+    <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good','Amazing' ]}/>
+    <StarRating color='salmon' size={24} className='test' defaultRating={3} />
+    <Test />
   </React.StrictMode>,
 )
