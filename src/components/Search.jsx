365f13761e19683122dc713fd6af0bc40f9cf487
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-12-15 19:00:52 +0300

    Refs to select DOM elements

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useRef } from "react"
 
 function Search({query, setQuery }) {
-  
+  const inputElement = useRef(null)
+
+  useEffect(function(){
+    function callback(e){
+      if(document.activeElement === inputElement.current) return //To not close the left Box when we hit 'Enter' more than once
+
+      if(e.code === 'Enter'){
+        inputElement.current.focus()
+        setQuery('')
+      }
+    }
+  document.addEventListener('keydown', callback)
+  inputElement.current.focus();
+
+  return ()=> document.removeEventListener('keydown', callback)
+  },[setQuery])
+
   return (
     <input
         className="search"
@@ -8,6 +25,7 @@ function Search({query, setQuery }) {
         placeholder="Search movies..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        ref={inputElement}
       />
   )
 }
