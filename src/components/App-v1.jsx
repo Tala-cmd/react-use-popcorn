8f4b343708342d2d2dbf80f0b8759896b17c2c93
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-12-17 12:48:50 +0300

    commit -3

--- src/components/App-v1.jsx
+++ src/components/App-v1.jsx
@@ -1,85 +1,85 @@
-/* eslint-disable no-unused-vars */
-import NavBar from "./NavBar";
-import Logo from "./Logo";
-import Search from "./Search";
-import NumResults from "./NumResults";
-import Main from "./Main";
-import Box from "./Box";
-import MovieList from "./MovieList";
-import WatchedSummary from "./WatchedSummary";
-import WatchedMoviesList from "./WatchedMoviesList";
-import StarRating from "./StarRating";
-import { useState } from "react";
+// /* eslint-disable no-unused-vars */
+// import NavBar from "./NavBar";
+// import Logo from "./Logo";
+// import Search from "./Search";
+// import NumResults from "./NumResults";
+// import Main from "./Main";
+// import Box from "./Box";
+// import MovieList from "./MovieList";
+// import WatchedSummary from "./WatchedSummary";
+// import WatchedMoviesList from "./WatchedMoviesList";
+// import StarRating from "./StarRating";
+// import { useState } from "react";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
+// const tempMovieData = [
+//   {
+//     imdbID: "tt1375666",
+//     Title: "Inception",
+//     Year: "2010",
+//     Poster:
+//         "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//   },
+//   {
+//     imdbID: "tt0133093",
+//     Title: "The Matrix",
+//     Year: "1999",
+//     Poster:
+//         "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+//   },
+//   {
+//     imdbID: "tt6751668",
+//     Title: "Parasite",
+//     Year: "2019",
+//     Poster:
+//         "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+//   },
+// ];
 
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
+// const tempWatchedData = [
+//   {
+//     imdbID: "tt1375666",
+//     Title: "Inception",
+//     Year: "2010",
+//     Poster:
+//         "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//     runtime: 148,
+//     imdbRating: 8.8,
+//     userRating: 10,
+//   },
+//   {
+//     imdbID: "tt0088763",
+//     Title: "Back to the Future",
+//     Year: "1985",
+//     Poster:
+//         "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+//     runtime: 116,
+//     imdbRating: 8.5,
+//     userRating: 9,
+//   },
+// ];
 
-export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+// export default function App() {
+//   const [movies, setMovies] = useState(tempMovieData);
+//   const [watched, setWatched] = useState(tempWatchedData);
 
-  return (
-    <>
-        <NavBar>
-          <Logo />
-          <Search />
-          <NumResults movies={movies} />
-        </NavBar>
+//   return (
+//     <>
+//         <NavBar>
+//           <Logo />
+//           <Search />
+//           <NumResults movies={movies} />
+//         </NavBar>
 
-        <Main data1={tempMovieData} data2={tempWatchedData}>
-          <Box data1={tempMovieData} data2={tempWatchedData} >
-            <MovieList movies={movies} />
-          </Box>
+//         <Main data1={tempMovieData} data2={tempWatchedData}>
+//           <Box data1={tempMovieData} data2={tempWatchedData} >
+//             <MovieList movies={movies} />
+//           </Box>
 
-          <Box>
-            <WatchedSummary watched={watched} />
-            <WatchedMoviesList watched={watched} />
-          </Box>
-        </Main>
-    </>
-  );
-}
+//           <Box>
+//             <WatchedSummary watched={watched} />
+//             <WatchedMoviesList watched={watched} />
+//           </Box>
+//         </Main>
+//     </>
+//   );
+// }
--- src/components/App-v2.jsx
+++ src/components/App-v2.jsx
@@ -1,180 +1,180 @@
-import NavBar from "./NavBar";
-import Logo from "./Logo";
-import Search from "./Search";
-import NumResults from "./NumResults";
-import Main from "./Main";
-import Box from "./Box";
-import MovieList from "./MovieList";
-import WatchedSummary from "./WatchedSummary";
-import WatchedMoviesList from "./WatchedMoviesList";
-import Loader from "./Loader";
-import MovieDetails from "./MovieDetails";
-import { useEffect, useState } from "react";
+// import NavBar from "./NavBar";
+// import Logo from "./Logo";
+// import Search from "./Search";
+// import NumResults from "./NumResults";
+// import Main from "./Main";
+// import Box from "./Box";
+// import MovieList from "./MovieList";
+// import WatchedSummary from "./WatchedSummary";
+// import WatchedMoviesList from "./WatchedMoviesList";
+// import Loader from "./Loader";
+// import MovieDetails from "./MovieDetails";
+// import { useEffect, useState } from "react";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
+// const tempMovieData = [
+//   {
+//     imdbID: "tt1375666",
+//     Title: "Inception",
+//     Year: "2010",
+//     Poster:
+//         "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//   },
+//   {
+//     imdbID: "tt0133093",
+//     Title: "The Matrix",
+//     Year: "1999",
+//     Poster:
+//         "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+//   },
+//   {
+//     imdbID: "tt6751668",
+//     Title: "Parasite",
+//     Year: "2019",
+//     Poster:
+//         "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+//   },
+// ];
 
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
+// const tempWatchedData = [
+//   {
+//     imdbID: "tt1375666",
+//     Title: "Inception",
+//     Year: "2010",
+//     Poster:
+//         "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//     runtime: 148,
+//     imdbRating: 8.8,
+//     userRating: 10,
+//   },
+//   {
+//     imdbID: "tt0088763",
+//     Title: "Back to the Future",
+//     Year: "1985",
+//     Poster:
+//         "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+//     runtime: 116,
+//     imdbRating: 8.5,
+//     userRating: 9,
+//   },
+// ];
 
-const KEY = '1b2dc3ef'
+// const KEY = '1b2dc3ef'
 
-export default function App() {
-  const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState('')
-  const [selectedId, setSelectedId] = useState(null)
+// export default function App() {
+//   const [query, setQuery] = useState("");
+//   const [movies, setMovies] = useState([]);
+//   const [watched, setWatched] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false)
+//   const [error, setError] = useState('')
+//   const [selectedId, setSelectedId] = useState(null)
 
-    /*
-      useEffect(function(){
-        console.log('After the initial render')
-      },[])
+//     /*
+//       useEffect(function(){
+//         console.log('After the initial render')
+//       },[])
 
-      useEffect(function(){
-        console.log('After every render')
-      })
+//       useEffect(function(){
+//         console.log('After every render')
+//       })
 
-      useEffect(function(){
-        console.log('When query is changed')
-      },[query])
+//       useEffect(function(){
+//         console.log('When query is changed')
+//       },[query])
 
-      console.log('During render')
-    */
+//       console.log('During render')
+//     */
     
-  function handleSelectMovie(id){
-    setSelectedId((selectedId)=> id === selectedId? null : id)
-  }
+//   function handleSelectMovie(id){
+//     setSelectedId((selectedId)=> id === selectedId? null : id)
+//   }
 
-  function handleCloseMovie(){
-    setSelectedId(null)
-  }
+//   function handleCloseMovie(){
+//     setSelectedId(null)
+//   }
 
-  function handleAddWatched(movie){
-    setWatched((watched)=> [...watched, movie])
-  }
+//   function handleAddWatched(movie){
+//     setWatched((watched)=> [...watched, movie])
+//   }
 
-  function handleDeleteWatched(id){
-    setWatched((watched)=> watched.filter((movie)=>
-      movie.imdbID !== id))
-  }
+//   function handleDeleteWatched(id){
+//     setWatched((watched)=> watched.filter((movie)=>
+//       movie.imdbID !== id))
+//   }
 
-  useEffect(function(){ 
-    const controller = new AbortController();
+//   useEffect(function(){ 
+//     const controller = new AbortController();
 
-    async function fetchMovies(){
-      try {
-        setIsLoading(true)
-        setError('')
-        const response = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-          { signal: controller.signal})
+//     async function fetchMovies(){
+//       try {
+//         setIsLoading(true)
+//         setError('')
+//         const response = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+//           { signal: controller.signal})
         
-        if(!response.ok) throw new Error('Something went wrong with fetching movies')
+//         if(!response.ok) throw new Error('Something went wrong with fetching movies')
   
-        const data= await response.json()
+//         const data= await response.json()
 
-        if(data.Response === 'False') throw new Error('Movie not found')
-        setMovies(data.Search)
-        console.log(data.Search)
-        // setIsLoading(false)
-        setError('')
-      } catch (err){
-        if(err.name !== 'AbortError'){
-          console.log(err.message)
-          setError(err.message)
-        }
-      } finally {
-        setIsLoading(false)
-      }
-    }
-    if(query.length < 3){
-      setMovies([])
-      setError('')
-      return 
-    }
-    handleCloseMovie();
-    fetchMovies();
+//         if(data.Response === 'False') throw new Error('Movie not found')
+//         setMovies(data.Search)
+//         console.log(data.Search)
+//         // setIsLoading(false)
+//         setError('')
+//       } catch (err){
+//         if(err.name !== 'AbortError'){
+//           console.log(err.message)
+//           setError(err.message)
+//         }
+//       } finally {
+//         setIsLoading(false)
+//       }
+//     }
+//     if(query.length < 3){
+//       setMovies([])
+//       setError('')
+//       return 
+//     }
+//     handleCloseMovie();
+//     fetchMovies();
 
-    return function(){
-      controller.abort();
-    }
-  } ,[query])
+//     return function(){
+//       controller.abort();
+//     }
+//   } ,[query])
 
   
-  return (
-    <>
-        <NavBar>
-          <Logo />
-          <Search query={query} setQuery={setQuery} />
-          <NumResults movies={movies} />
-        </NavBar>
+//   return (
+//     <>
+//         <NavBar>
+//           <Logo />
+//           <Search query={query} setQuery={setQuery} />
+//           <NumResults movies={movies} />
+//         </NavBar>
 
-        <Main data1={tempMovieData} data2={tempWatchedData}>
-          <Box data1={tempMovieData} data2={tempWatchedData} >
-            {isLoading && <Loader />}
+//         <Main data1={tempMovieData} data2={tempWatchedData}>
+//           <Box data1={tempMovieData} data2={tempWatchedData} >
+//             {isLoading && <Loader />}
             
-            {!isLoading && !error && 
-              <MovieList movies={movies} onSelectMovie={handleSelectMovie}/>}
-          </Box>
+//             {!isLoading && !error && 
+//               <MovieList movies={movies} onSelectMovie={handleSelectMovie}/>}
+//           </Box>
 
-          <Box>
-            { selectedId ?
-            <MovieDetails 
-              selectedId={selectedId} 
-              onCloseMovie={handleCloseMovie} 
-              onAddWatched={handleAddWatched}
-              watched={watched}
-              /> 
-            : <>
-              <WatchedSummary watched={watched} />
-              <WatchedMoviesList watched={watched} onDeleteWatched={handleDeleteWatched} />
-            </>
-            }
+//           <Box>
+//             { selectedId ?
+//             <MovieDetails 
+//               selectedId={selectedId} 
+//               onCloseMovie={handleCloseMovie} 
+//               onAddWatched={handleAddWatched}
+//               watched={watched}
+//               /> 
+//             : <>
+//               <WatchedSummary watched={watched} />
+//               <WatchedMoviesList watched={watched} onDeleteWatched={handleDeleteWatched} />
+//             </>
+//             }
             
-          </Box>
-        </Main>
-    </>
-  );
-}
+//           </Box>
+//         </Main>
+//     </>
+//   );
+// }
